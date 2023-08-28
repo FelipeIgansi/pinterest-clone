5da93f0ca769feea09eed39f610095f32ba1b50a
FelipeIgansi <lipeigansi@gmail.com>
2023-08-28 18:26:48 -0300

    Adição e formatação de imagens e da legenda de cada uma

--- app/src/main/java/com/pinterest/pinterest_clone/MainActivity.kt
+++ app/src/main/java/com/pinterest/pinterest_clone/MainActivity.kt
@@ -3,23 +3,16 @@ package com.pinterest.pinterest_clone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -28,11 +21,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.pinterest.pinterest_clone.R.drawable.pinterest01
+import com.pinterest.pinterest_clone.R.drawable.pinterest02
+import com.pinterest.pinterest_clone.R.drawable.pinterest03
+import com.pinterest.pinterest_clone.R.drawable.pinterest04
+import com.pinterest.pinterest_clone.R.drawable.pinterest05
+import com.pinterest.pinterest_clone.R.drawable.pinterest06
+import com.pinterest.pinterest_clone.R.drawable.pinterest07
+import com.pinterest.pinterest_clone.R.drawable.pinterest08
+import com.pinterest.pinterest_clone.R.drawable.pinterest09
+import com.pinterest.pinterest_clone.R.drawable.pinterest10
 import com.pinterest.pinterest_clone.ui.theme.PinterestcloneTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,21 +47,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    callListHome()
+                    CallListHome()
                 }
             }
         }
-    };
+    }
 }
 
 
 @Composable
-fun ListHome(items: List<String>, caption: List<String>) {
+fun ListHome(items: List<Int>, caption: List<String>) {
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(16.dp)
     ) {
-        items.chunked(2) { rowItems ->
+        items.chunked(2).forEach { rowItems ->
             item {
                 Row(
                     modifier = Modifier
@@ -74,14 +76,16 @@ fun ListHome(items: List<String>, caption: List<String>) {
                                 .padding(4.dp),
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
-                            Box(
+                            Image(
+                                painter = painterResource(id = item),
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
                                 modifier = Modifier
                                     .clip(RoundedCornerShape(16.dp))
                                     .fillMaxSize()
-                                    .aspectRatio(0.5f)
-                                    .background(Color.Gray)
+                                    .aspectRatio(0.7f)
                             )
-                            Text(text = caption[rowItems.indexOf(item)])
+                            Text(text = caption[items.indexOf(item)])
                         }
                     }
                 }
@@ -94,14 +98,26 @@ fun ListHome(items: List<String>, caption: List<String>) {
 @Composable
 fun PinterestClonePreview() {
     PinterestcloneTheme {
-        callListHome()
+        CallListHome()
     }
 }
 
 
 @Composable
-private fun callListHome() {
-    val itemList = List(20) { index -> "Item ${index + 1}" }
-    val captions = List(20) { index -> "Caption ${index + 1}" }
-    ListHome(items = itemList, caption = captions)
+private fun CallListHome() {
+    val captions = List(10) { index -> "Caption ${index + 1}" }
+    ListHome(
+        items = listOf(
+            pinterest01,
+            pinterest02,
+            pinterest03,
+            pinterest04,
+            pinterest05,
+            pinterest06,
+            pinterest07,
+            pinterest08,
+            pinterest09,
+            pinterest10,
+        ), caption = captions
+    )
 }
